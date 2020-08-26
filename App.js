5f44888a0154156024f2fc0eb5bcc5a9045e52bd
Adriana Rodriguez <agrodriguezs09@gmail.com>
2020-08-26 18:05:05 +0200

    guardando en el localStorage

--- App.js
+++ App.js
@@ -6,6 +6,14 @@ const App = new Vue({
         nuevaTarea: null,
         mensajeError: null
     },
+    created(){
+        let datosDB = JSON.parse(localStorage.getItem('tareas-vue'));
+        id (datosDB === null){
+            this.tareas = [];
+        }else {
+            this.tareas = datosDB;
+        }
+    },
     methods:{
         adicionarTarea(){
             if (this.nuevaTarea && this.nuevaTarea.length >2){
@@ -15,17 +23,18 @@ const App = new Vue({
                     status: false
                 });
                 this.nuevaTarea = '';
+                localStorage.setItem('tareas-vue', JSON.stringify(this.tareas));
             }else{
                 this.mensajeError = "Escribe una tarea concreta";   
             }
-          
         },
         modificarTarea(index){
          this.tareas[index].status = !this.tareas[index].status;
+         localStorage.setItem('tareas-vue', JSON.stringify(this.tareas));
         },
         eliminarTarea(index){
-
           this.tareas.splice(index, 1);
+          localStorage.setItem('tareas-vue', JSON.stringify(this.tareas));
         }
     }
 });
